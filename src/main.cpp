5c02f182caee6490a4f8b7be0abcba349843d59d
Javier <ibrahimcruz06@gmail.com>
2023-09-17 18:52:14 -0600

    Tarea terminada

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,37 @@
 #include <iostream> 
-#include "Ordenador.h" 
+#include "Ordenador.h"  
+#include <cstdlib> 
+#include <ctime> 
+#include <chrono>
 
 int main(){ 
 
-    int tamano = 5; 
-    int arreglo[tamano] = {-13, 26, -45, 78, 5}; 
+    srand(time(nullptr)); 
+
+    int tamano = 200000; 
+    int arreglo[tamano]; 
+
+    for(int i=0; i<tamano; i++){ 
+        arreglo[i] = rand() - RAND_MAX /2; 
+    }
 
     Ordenador ordenador; 
 
+    auto start_time = std::chrono::high_resolution_clock::now();
+
     ordenador.mergesort(arreglo, tamano); 
 
-    for(int i=0; i<tamano; i++){ 
-        std::cout<<arreglo[i]<<std::endl; 
+    auto end_time = std::chrono::high_resolution_clock::now(); 
+
+    auto tiempoEjecucion = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+
+    std::cout<<tiempoEjecucion.count()<<std::endl; 
+
+    for(int i=1; i<tamano; i++){
+        if(arreglo[i] < arreglo[i-1]){
+            std::cout<<"Failed!"; 
+        }
     }
- 
 
     return 0; 
 }
